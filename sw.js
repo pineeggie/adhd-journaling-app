0ba9551938a7d7955257081b7657c7ce3c98713d
pineeggie <pineeggie@users.noreply.github.com>
2026-07-16 13:48:01 +0900

    Move welcome content above timeline

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "murmur-v5";
+const CACHE = "murmur-v6";
 const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
