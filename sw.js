dcfd25a0fd49310e9223b84151fc8a86fa3477f1
pineeggie <pineeggie@users.noreply.github.com>
2026-07-16 13:50:36 +0900

    Bust stylesheet cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "murmur-v6";
-const ASSETS = ["./", "./index.html", "./styles.css", "./app.js", "./manifest.webmanifest", "./icon.svg"];
+const CACHE = "murmur-v7";
+const ASSETS = ["./", "./index.html", "./styles.css?v=6", "./app.js", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
